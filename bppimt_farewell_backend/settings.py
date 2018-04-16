5ddebbb4fc2a83a1faca944139e01d989f9380c5
Ubuntu <ubuntu@ip-172-31-22-152.ap-south-1.compute.internal>
2018-04-16 19:34:03 +0000

    After running collectstatic command from manage.py

--- bppimt_farewell_backend/settings.py
+++ bppimt_farewell_backend/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
 #django_comments settings
 
