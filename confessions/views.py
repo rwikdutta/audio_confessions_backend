f12f7a1fb8c8b90223a286db7dd88b9cdfbbeab6
rwikdutta <rwikdutta@outlook.com>
2018-04-29 22:42:07 +0530

    Corrected ConfessionStudentFilterView queryset

--- confessions/views.py
+++ confessions/views.py
@@ -71,7 +71,7 @@ class AddConfessionView(views.APIView):
             return Response({'error':True,'message':'Error Occured','error_fields':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
 
 class ConfessionStudentFilterView(generics.ListAPIView):
-    queryset = Confessions.objects.filter(is_anonymous=False).order_by('-id')
+    queryset = Confessions.objects.filter(is_anonymous=False,is_approved=True).order_by('-id')
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('student__id',)
     serializer_class = ConfessionsSerializer
