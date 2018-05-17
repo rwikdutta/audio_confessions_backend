145751ea6bfaaccb1d6dc1f568293ad4d93c1eac
rwikdutta <rwikdutta@outlook.com>
2018-05-17 14:16:27 +0530

    Removed redundant comment

--- bppimt_farewell_backend/settings.py
+++ bppimt_farewell_backend/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-#TODO: Make the secret_key an environment variable
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('BPP_FAREWELL_SECRET_KEY', '')
